58e3a6d26e6a8739fee435d4cf666d797fa7a5d5
William Tong <williamtong105@gmail.com>
2025-02-13 11:37:29 -0500

    Checkpoint changes

--- task/graph.py
+++ task/graph.py
@@ -2,6 +2,7 @@
 
 # <codecell>
 import jax
+import jax.numpy as jnp
 import numpy as np
 
 def fast_binary_pow(a, n):
@@ -15,6 +16,16 @@ def _bin_path(a):
     c = (c > 0).astype(int)
     return c
 
+def samp_child(node, dist):
+    low = node * 2**dist
+    high = node * 2**dist + 2**dist
+    return np.random.randint(low, high)
+
+key = jax.random.PRNGKey(9)
+jax.random.randint(key, (3,), jnp.array([1, 10, 100]), jnp.array([9, 90, 900]))
+# TODO: use jax to sample children <-- STOPPED HERE
+
+# <codecell>
 
 class GraphTiTask:
     def __init__(self, n_nodes, n_dims=None, p_connect=None, prob_thresh=(0.1, 0.7), samp_adj=True, seed=None, batch_size=128) -> None:
@@ -32,27 +43,27 @@ class GraphTiTask:
 
         self.rng = np.random.default_rng(seed)
 
-        # do_cont = False
-        # while not do_cont:
-        #     self.adj = self.rng.binomial(n=1, p=self.p_connect, size=self.n_nodes**2).reshape(self.n_nodes, self.n_nodes)
-        #     self.cnx = fast_binary_pow(self.adj, n_nodes)
+        do_cont = False
+        while not do_cont:
+            self.adj = self.rng.binomial(n=1, p=self.p_connect, size=self.n_nodes**2).reshape(self.n_nodes, self.n_nodes)
+            self.cnx = fast_binary_pow(self.adj, n_nodes)
 
-        #     p = np.mean(self.cnx)
-        #     if prob_thresh[0] < p < prob_thresh[1]:
-        #         do_cont = True
-        #     else:
-        #         print(f'warn: rejecting graph with p={p:.2f}')
+            p = np.mean(self.cnx)
+            if prob_thresh[0] < p < prob_thresh[1]:
+                do_cont = True
+            else:
+                print(f'warn: rejecting graph with p={p:.2f}')
 
         ## Enforced binary tree
-        self.adj = np.zeros((self.n_nodes, self.n_nodes))
-        idx = 2
-        while idx < self.n_nodes:
-            row = idx // 2 - 1
-            self.adj[row,idx-1] += 1
-            self.adj[row,idx] += 1
-            idx += 2
+        # self.adj = np.zeros((self.n_nodes, self.n_nodes))
+        # idx = 2
+        # while idx < self.n_nodes:
+        #     row = idx // 2 - 1
+        #     self.adj[row,idx-1] += 1
+        #     self.adj[row,idx] += 1
+        #     idx += 2
 
-        self.cnx = fast_binary_pow(self.adj, n_nodes)
+        # self.cnx = fast_binary_pow(self.adj, n_nodes)
         
         self.pat = 2 * self.cnx - self.adj
         idx = np.arange(self.n_nodes**2)
@@ -69,7 +80,6 @@ class GraphTiTask:
 
         self.rng = np.random.default_rng(None)
 
-    
     def __next__(self):
         pos_idx = self.pos_idx_adj if self.samp_adj else self.pos_idx_nadj
         xs_pos = self.rng.choice(pos_idx, replace=True, size=self.batch_size // 2)
@@ -93,6 +103,73 @@ class GraphTiTask:
     def __iter__(self):
         return self
 
+
+class BinaryTreeTiTask:
+    def __init__(self, depth, n_dims=None, samp_dist=1, batch_size=128) -> None:
+        assert batch_size % 2 == 0, 'batch size must be even'
+
+        self.depth = depth
+        # self.n_nodes = 2**depth - 1
+        self.n_dims = n_dims
+        self.samp_dist = samp_dist
+        self.batch_size = batch_size
+
+        # self.adj = np.zeros((self.n_nodes, self.n_nodes))
+        # idx = 2
+        # while idx < self.n_nodes:
+        #     row = idx // 2 - 1
+        #     self.adj[row,idx-1] += 1
+        #     self.adj[row,idx] += 1
+        #     idx += 2
+
+        # self.cnx = fast_binary_pow(self.adj, self.n_nodes)
+        
+        # self.pat = 2 * self.cnx - self.adj
+        # idx = np.arange(self.n_nodes**2)
+        # neg_sel = self.pat.ravel() == 0
+        # pos_sel_adj = self.pat.ravel() == 1
+        # pos_sel_nadj = self.pat.ravel() == 2
+
+        # self.pos_idx_adj = idx[pos_sel_adj]
+        # self.pos_idx_nadj = idx[pos_sel_nadj]
+        # self.neg_idx = idx[neg_sel]
+
+        # if n_dims is not None:
+        #     self.emb_mat = self.rng.standard_normal((self.n_nodes, self.n_dims)) / np.sqrt(self.n_dims)
+
+        # self.rng = np.random.default_rng(None)
+
+    
+    def __next__(self):
+        root_depths = np.random.choice(self.depth - self.samp_dist, size=self.batch_size)
+        range_low = 2**root_depths
+        range_high = 2**(root_depths + 1) - 1
+
+
+
+
+
+        pos_idx = self.pos_idx_adj if self.samp_adj else self.pos_idx_nadj
+        xs_pos = self.rng.choice(pos_idx, replace=True, size=self.batch_size // 2)
+        xs_neg = self.rng.choice(self.neg_idx, replace=True, size=self.batch_size // 2)
+
+        xs = np.concat((xs_pos, xs_neg))
+        ys = np.zeros(self.batch_size)
+        ys[:self.batch_size//2] = 1
+
+        shuffle_idx = self.rng.choice(self.batch_size, size=self.batch_size, replace=False)
+        xs = xs[shuffle_idx]
+        ys = ys[shuffle_idx]
+
+        xs = np.stack((xs // self.n_nodes, xs % self.n_nodes), axis=1)
+
+        if self.n_dims is not None:
+            xs = self.emb_mat[xs]
+
+        return xs, ys
+
+    def __iter__(self):
+        return self
 
 # task = GraphTiTask(31, n_dims=None, batch_size=6, samp_adj=False)
 # print(np.mean(task.cnx > 0))
