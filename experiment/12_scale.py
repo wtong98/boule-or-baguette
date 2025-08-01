Fix muP scaling

experiment/12_scale.py
@@ -19,11 +19,11 @@ depth = 10
 n_hidden = 128
 batch_size = 128
 
-cot = True
+cot = False
 ttr = True
 nouveau = True
 force_bin_label = True
-n_arms = 10
+n_arms = 100
 n_hop = 5
 test_n_hop = 7
 
@@ -92,6 +92,8 @@ def summarize(state):
     # a = state.params['Dense_1']['kernel']
     return a
 
+lr = 1e-2
+gamma = 10
 
 state, hist = train(config,
                     train_iter=iter(train_task), 
@@ -105,8 +107,9 @@ state, hist = train(config,
                     eval_fns=None,
                     # print_fn=print_gen if cot else None,
                     print_fn=None,
-                    lr=1e-2,
-                    optim=optax.sign_sgd,
+                    lr=lr * gamma,
+                    gamma=gamma,
+                    optim=optax.adamw,
                     summary_fn=summarize
                     )
 
@@ -578,8 +581,8 @@ plt.hist(proj[4,:], bins=50)
 
 
 # <codecell>
-df = collate_dfs('remote/12_scale/zero_arm', show_progress=True)
-# df = collate_dfs('remote/12_scale/ar_arm', show_progress=True)
+# df = collate_dfs('remote/12_scale/zero_arm', show_progress=True)
+df = collate_dfs('remote/12_scale/ar_arm', show_progress=True)
 # df = collate_dfs('remote/12_scale/arm', show_progress=True)
 df
 
@@ -592,7 +595,7 @@ def extract_plot_vals(row):
         row['config']['n_hidden'],
         row['config']['residual_connections'],
         row['train_args']['lr'],
-        row['hist']['test'][10]['acc'].item(),
+        row['hist']['test'][-1]['acc'].item(),
         row['info'],
     ], index=['name', 'n_arms', 'n_hop', 'n_hidden', 'resid', 'lr', 'acc_hist', 'info'])
 
@@ -619,21 +622,22 @@ plot_df
 # for name in np.unique(plot_df['name']):
 mdf = plot_df.copy()
 mdf = mdf[(mdf['test_n_hop'] == 5)
-        #   & (mdf['lr'] == 1e-3)
+          & (mdf['lr'] == 1e-2)
         #   & (mdf['resid'] == False)
           ]
 
-mdf = mdf[['n_arms', 'n_hidden', 'acc_hist']]
-mdf = mdf.groupby(['n_arms', 'n_hidden'], as_index=False).max()
-mdf = mdf.pivot(index='n_arms', columns='n_hidden', values='acc_hist')
+mdf = mdf[['n_arms', 'n_hidden', 'acc']]
+mdf = mdf.groupby(['n_arms', 'n_hidden'], as_index=False).mean()
+mdf = mdf.pivot(index='n_arms', columns='n_hidden', values='acc')
 
 mdf = mdf.iloc[::-1]
 
 g = sns.heatmap(mdf, square=False, vmin=0.6, vmax=0.9)
+# g = sns.heatmap(mdf, square=False, vmin=0.5, vmax=1)
 
 xs = 2**np.linspace(-5, 8)
-g.plot(xs, 48 - 0.5 * xs, color='cyan', linestyle='dashed')
-g.plot(xs, 35 - 1 * xs, color='cyan', linestyle='dashed')
+g.plot(xs, 55 - 1 * xs, color='cyan', linestyle='dashed')
+g.plot(xs, 45 - 2 * xs, color='cyan', linestyle='dashed')
 # g.plot(xs, 50 - 2 * xs, color='cyan', linestyle='dashed')
 
 # g.plot(xs, 39 - xs, color='gray', linestyle='dashed')
@@ -707,20 +711,21 @@ mdf = mdf[
     & (mdf['residual_connections'] == False)
     ]
 
-mdf = mdf[['depth', 'n_hidden', 'acc_hist']]
+mdf = mdf[['depth', 'n_hidden', 'acc']]
 mdf = mdf.groupby(['depth', 'n_hidden'], as_index=False).max()
-mdf = mdf.pivot(index='depth', columns='n_hidden', values='acc_hist')
+mdf = mdf.pivot(index='depth', columns='n_hidden', values='acc')
 
 mdf = mdf.iloc[::-1]
 
 g = sns.heatmap(mdf, square=False, vmin=0.6, vmax=0.9)
+# g = sns.heatmap(mdf, square=False, vmin=0.5, vmax=1)
 
 xs = 2**np.linspace(-5, 8)
 # g.plot(xs, 35 - 0.7 * xs, color='cyan', linestyle='dashed')
 # g.plot(xs, 50 - 1.5 * xs, color='cyan', linestyle='dashed')
 # g.plot(xs + np.log(xs), 40 - 1 * xs, color='cyan', linestyle='dashed')
 # g.plot(xs, 20 - 0.67 * xs, color='cyan', linestyle='dashed')
-g.plot(xs, 20 - 1 * xs, color='cyan', linestyle='dashed')
+g.plot(xs, 40 - 1 * xs, color='cyan', linestyle='dashed')
 g.plot(xs, 17 - 0.5 * xs, color='cyan', linestyle='dashed')
 g.plot(xs, 17 - 0.33 * xs, color='cyan', linestyle='dashed')
 
@@ -763,9 +768,10 @@ def extract_plot_vals(row):
         row['train_task'].depth,
         row['train_task'].samp_dist[1],
         row['config']['n_hidden'],
+        row['hist']['test'][25]['acc'].item(),
         n_hop_prop,
         row['info'],
-    ], index=['name', 'n_arms', 'depth', 'n_hop', 'n_hidden', 'n_hop_prop', 'info'])
+    ], index=['name', 'n_arms', 'depth', 'n_hop', 'n_hidden', 'acc_hist', 'n_hop_prop', 'info'])
 
 plot_df = df.apply(extract_plot_vals, axis=1) \
             .reset_index(drop=True) \
@@ -792,16 +798,17 @@ mdf = plot_df.copy()
 mdf = mdf[
     (mdf['test_n_hop'] == 0.5)
     & (mdf['n_hop_prop'] == 0.5)
-    & (mdf['n_arms'] == 2)
+    & (mdf['n_arms'] == 10)
     ]
 
-mdf = mdf[['depth', 'n_hidden', 'acc']]
+mdf = mdf[['depth', 'n_hidden', 'acc_hist']]
 mdf = mdf.groupby(['depth', 'n_hidden'], as_index=False).mean()
-mdf = mdf.pivot(index='depth', columns='n_hidden', values='acc')
+mdf = mdf.pivot(index='depth', columns='n_hidden', values='acc_hist')
 
 mdf = mdf.iloc[::-1]
 
-g = sns.heatmap(mdf, square=False, vmin=0.5, vmax=1)
+g = sns.heatmap(mdf, square=False, vmin=0.6, vmax=0.9)
+# g = sns.heatmap(mdf, square=False, vmin=0.5, vmax=1)
 
 xs = 2**np.linspace(-5, 8)
 # g.plot(xs, 35 - 0.7 * xs, color='cyan', linestyle='dashed')
@@ -810,7 +817,7 @@ xs = 2**np.linspace(-5, 8)
 # g.plot(xs, 45 - 0.67 * xs, color='cyan', linestyle='dashed')
 # g.plot(xs, 45 - 0.5 * xs, color='cyan', linestyle='dashed')
 
-g.plot(xs, 20 - 0.5 * xs, color='cyan', linestyle='dashed')
+g.plot(xs, 40 - 0.5 * xs, color='cyan', linestyle='dashed')
 
 g.set_ylabel('depth')
 g.set_xlabel('n_hidden')

experiment/remote/12_scale/ar_arm/run.py
@@ -31,7 +31,8 @@ switch = [True]  # resid
 n_arms = (2**np.linspace(3, 9, num=40)).astype(int) * 2
 n_widths = (2**np.linspace(3, 9, num=40)).astype(int) * 2
 
-lrs = [1e-2, 5e-3, 1e-3]
+lrs = [1e-2]
+gammas = [1, 10, 100]
 
 ### START TEST CONFIGS
 # run_split = 1
@@ -48,13 +49,14 @@ lrs = [1e-2, 5e-3, 1e-3]
 # n_widths = [256]
 
 # lrs = [1e-2]
+# gammas = [10]
 ### END TEST CONFIGS
 
 all_cases = []
 
 eval_fns = [loss_and_acc, gen_acc_cot1]
 
-for lr, n_arm, n_hidden in itertools.product(lrs, n_arms, n_widths):
+for lr, gamma, n_arm, n_hidden in itertools.product(lrs, gammas, n_arms, n_widths):
     n_vocab = n_arm * depth + 1 + StarfishTask.offset
 
     model_args = {
@@ -69,7 +71,8 @@ for lr, n_arm, n_hidden in itertools.product(lrs, n_arms, n_widths):
             'loss': loss,
             'test_every': 1000,
             'train_iters': train_iters,
-            'lr': lr
+            'lr': lr * gamma,
+            'gamma': gamma,
             # 'lr': optax.schedules.warmup_cosine_decay_schedule(
             #     init_value=1e-3,
             #     peak_value=1e-2,

experiment/remote/12_scale/ar_length/run.py
@@ -27,7 +27,7 @@ n_arms = [10]
 n_depths = (2**np.linspace(3, 9, num=40)).astype(int) * 2
 n_widths = (2**np.linspace(3, 9, num=40)).astype(int) * 2
 
-# n_hop_props = [0.25, 0.5, 0.7]
+test_n_hop_props = [0.25, 0.5, 0.7, 0.95]
 n_hop_props = [0.5]
 
 ### START TEST CONFIGS
@@ -135,7 +135,7 @@ for case in tqdm(all_cases):
     print('RUNNING', case.name)
     case.run()
 
-    for n_hop_prop in n_hop_props:
+    for n_hop_prop in test_n_hop_props:
         tt = case.train_task
         n_hop = np.round(tt.depth * n_hop_prop).astype(int)
         test_task = StarfishTask(n_arms=tt.n_arms, depth=tt.depth, samp_dist=n_hop, cot=tt.cot, trace_to_start=tt.trace_to_start, nouveau=tt.nouveau, batch_size=tt.batch_size, force_bin_label=tt.force_bin_label)

experiment/remote/12_scale/zero_arm/run.py
@@ -25,7 +25,7 @@ n_hop = 5
 n_hops_test = [5, 6, 7, 8, 9]
 
 all_n_layer = [1]
-switch = [False, True]  # resid
+switch = [False]  # resid
 all_lrs = [1e-2, 1e-3]
 
 n_arms = (2**np.linspace(3, 9, num=40)).astype(int) * 2
@@ -107,6 +107,7 @@ for lr, resid, n_arm, n_hidden in itertools.product(all_lrs, switch, n_arms, n_w
                                 residual_connections=resid,
                                 mup_scale=True,
                                 linear_att=False,
+                                unif_att=True,
                                 **model_args),
                 train_args=make_train_args('bce'),
                 train_task=make_chain(cot=False)

experiment/remote/12_scale/zero_length/run.py
@@ -25,7 +25,7 @@ n_arms = [10]
 n_depths = (2**np.linspace(3, 9, num=40)).astype(int) * 2
 n_widths = (2**np.linspace(3, 9, num=40)).astype(int) * 2
 
-# n_hop_props = [0.25, 0.5, 0.7, 0.95]
+test_n_hop_props = [0.25, 0.5, 0.7, 0.95]
 n_hop_props = [0.5]
 lrs = [1e-3]
 switch = [False]
@@ -100,6 +100,7 @@ for lr, use_resid, n_hop_prop, n_arm, depth, n_hidden in itertools.product(lrs, 
                                 residual_connections=use_resid,
                                 mup_scale=True,
                                 linear_att=False,
+                                unif_att=True,
                                 **model_args),
                 train_args=make_train_args('bce'),
                 train_task=make_chain(cot=False),
@@ -115,7 +116,7 @@ for case in tqdm(all_cases):
     print('RUNNING', case.name)
     case.run()
 
-    for n_hop_prop in n_hop_props:
+    for n_hop_prop in test_n_hop_props:
         tt = case.train_task
         n_hop = np.round(tt.depth * n_hop_prop).astype(int)
         test_task = StarfishTask(n_arms=tt.n_arms, depth=tt.depth, samp_dist=n_hop, cot=tt.cot, trace_to_start=tt.trace_to_start, nouveau=tt.nouveau)

model/transformer.py
@@ -129,8 +129,10 @@ class SimpleSelfAttention(nn.Module):
         head_dim = n_feats // n_heads
 
         if self.config.mup_scale:
-            kernel_init = nn.initializers.normal(1)
-            prefac = 1 / np.sqrt(head_dim)
+            # kernel_init = nn.initializers.normal(1)
+            # prefac = 1 / np.sqrt(head_dim)
+            kernel_init = nn.initializers.normal(np.sqrt(head_dim))
+            prefac = 1 / head_dim
         else:
             kernel_init = nn.initializers.truncated_normal(1 / np.sqrt(head_dim))
             prefac = 1
@@ -157,8 +159,10 @@ class SimpleSelfAttention(nn.Module):
         out = jnp.einsum('...hqk,...khd->...qhd', attn_weights, value)
 
         if self.config.mup_scale:
-            kernel_init = nn.initializers.normal(1)
-            prefac = 1 / np.sqrt(self.config.n_hidden)
+            # kernel_init = nn.initializers.normal(1)
+            # prefac = 1 / np.sqrt(self.config.n_hidden)
+            kernel_init = nn.initializers.normal(np.sqrt(self.config.n_hidden))
+            prefac = 1 / self.config.n_hidden
         else:
             kernel_init = nn.initializers.truncated_normal(1 / np.sqrt(self.config.n_hidden))
             prefac = 1
@@ -193,8 +197,10 @@ class TransformerBlock(nn.Module):
             x = nn.LayerNorm()(x)
         
         if self.config.mup_scale:
-            kernel_init = nn.initializers.normal(1)
-            prefac = 1 / np.sqrt(self.config.n_hidden)
+            # kernel_init = nn.initializers.normal(1)
+            # prefac = 1 / np.sqrt(self.config.n_hidden)
+            kernel_init = nn.initializers.normal(np.sqrt(self.config.n_hidden))
+            prefac = 1 / self.config.n_hidden
         else:
             kernel_init = nn.initializers.truncated_normal(1 / np.sqrt(self.config.n_hidden))
             prefac = 1
@@ -238,6 +244,7 @@ class Transformer(nn.Module):
             y = nn.Embed(
                 num_embeddings=self.config.n_vocab,
                 features=self.config.n_hidden,
+                embedding_init=nn.initializers.normal(1),
                 # features=4096,
                 name=name)(y)
 
@@ -412,7 +419,7 @@ class Tr(nn.Module):
         return x
 
 
-## COORDINATE CHECKING
+### COORDINATE CHECKING
 # import matplotlib.pyplot as plt
 
 # import sys
@@ -438,10 +445,8 @@ class Tr(nn.Module):
 #     curr_norms_att = []
 
 #     for n_hidden in [64, 128, 256, 512, 1024, 2048]:
-#         # gamma0 = 1
-#         # gamma = gamma0 * np.sqrt(n_hidden)
-#         # lr = gamma0**2 * base_lr
-#         lr = base_lr
+#         gamma = 100
+#         lr = base_lr * gamma
 
 
 #         config = TransformerConfig(n_vocab=n_vocab,
@@ -464,7 +469,8 @@ class Tr(nn.Module):
 #                                 model=config.to_model(),
 #                                 dummy_input=xs_init,
 #                                 lr=lr,
-#                                 optim=optax.sign_sgd)
+#                                 optim=optax.sign_sgd,
+#                                 gamma=gamma)
 
 #         # logits_init = state.apply_fn({'params': state.params}, xs_init)
 #         logits_init, intm = config.to_model().apply({'params': state.params}, xs_init, mutable='intermediates')
@@ -478,7 +484,8 @@ class Tr(nn.Module):
 #                             test_every=1000,
 #                             train_iters=n_steps, 
 #                             test_iters=1,
-#                             seed=None)
+#                             seed=None,
+#                             gamma=gamma)
 
 #         logits, intm = config.to_model().apply({'params': state.params}, xs_init, mutable='intermediates')
 #         # att = intm['intermediates']['TransformerBlock_1']['SimpleSelfAttention_0']['attention_logits'][0]
@@ -495,14 +502,11 @@ class Tr(nn.Module):
     
 #     all_norms.append(curr_norms)
 
-# # <codecell>
+
 # for norms in all_norms:
 #     norms = np.array(norms)
 #     plt.plot(norms[:,0], 'o--')
-#     plt.plot(norms[:,1], 'o--')
+#     # plt.plot(norms[:,1], 'o--')
 #     plt.plot(norms[:,2], 'o--')
 #     # plt.yscale('log')
 
-
-# # <codecell>
-# # plt.plot(curr_norms_att, 'o--')