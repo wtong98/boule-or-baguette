Fix arm scaling run

experiment/12_scale.py
@@ -15,7 +15,7 @@ from model.mlp import MlpConfig
 from model.transformer import *
 from task.graph import *
 
-depth = 100
+depth = 10
 n_hidden = 128
 batch_size = 128
 
@@ -23,7 +23,7 @@ cot = True
 ttr = True
 nouveau = True
 force_bin_label = True
-n_arms = 10
+n_arms = 100
 n_hop = 5
 test_n_hop = 7
 
@@ -581,8 +581,8 @@ plt.hist(proj[4,:], bins=50)
 
 
 # <codecell>
-df = collate_dfs('remote/12_scale/zero_arm', show_progress=True)
-# df = collate_dfs('remote/12_scale/ar_arm', show_progress=True)
+# df = collate_dfs('remote/12_scale/zero_arm', show_progress=True)
+df = collate_dfs('remote/12_scale/ar_arm', show_progress=True)
 # df = collate_dfs('remote/12_scale/arm', show_progress=True)
 df
 
@@ -623,19 +623,19 @@ plot_df
 # for name in np.unique(plot_df['name']):
 mdf = plot_df.copy()
 mdf = mdf[(mdf['test_n_hop'] == 5)
-          & (mdf['lr'] == 1e-2)
+        #   & (mdf['lr'] == 1e-2)
         #   & (mdf['gamma'] == 1)
         #   & (mdf['resid'] == False)
           ]
 
-mdf = mdf[['n_arms', 'n_hidden', 'acc_hist']]
+mdf = mdf[['n_arms', 'n_hidden', 'acc']]
 mdf = mdf.groupby(['n_arms', 'n_hidden'], as_index=False).mean()
-mdf = mdf.pivot(index='n_arms', columns='n_hidden', values='acc_hist')
+mdf = mdf.pivot(index='n_arms', columns='n_hidden', values='acc')
 
 mdf = mdf.iloc[::-1]
 
-g = sns.heatmap(mdf, square=False, vmin=0.6, vmax=0.9)
-# g = sns.heatmap(mdf, square=False, vmin=0.5, vmax=1)
+# g = sns.heatmap(mdf, square=False, vmin=0.6, vmax=0.9)
+g = sns.heatmap(mdf, square=False, vmin=0.5, vmax=1)
 
 xs = 2**np.linspace(-5, 8)
 g.plot(xs, 40 - 1 * xs, color='cyan', linestyle='dashed')
@@ -651,7 +651,7 @@ g.set_ylabel('n_arms')
 g.set_xlabel('n_hidden')
 
 plt.title('Zero short training')
-plt.savefig(f'fig/zero_mlp_arms_v_size_short.png', bbox_inches='tight')
+# plt.savefig(f'fig/zero_mlp_arms_v_size_short.png', bbox_inches='tight')
 # plt.savefig(f'fig/ar_mlp_arms_v_size_long.png', bbox_inches='tight')
 plt.show()
 
@@ -798,7 +798,7 @@ plot_df
 # for name in np.unique(plot_df['name']):
 mdf = plot_df.copy()
 mdf = mdf[
-    (mdf['test_n_hop'] == 0.5)
+    (mdf['test_n_hop'] == 0.7)
     & (mdf['n_hop_prop'] == 0.5)
     & (mdf['n_arms'] == 10)
     ]
@@ -815,17 +815,17 @@ g = sns.heatmap(mdf, square=False, vmin=0.6, vmax=0.9)
 xs = 2**np.linspace(-5, 8)
 # g.plot(xs, 35 - 0.7 * xs, color='cyan', linestyle='dashed')
 # g.plot(xs, 50 - 1.5 * xs, color='cyan', linestyle='dashed')
-# g.plot(xs, 20 - 1 * xs, color='cyan', linestyle='dashed')
+g.plot(xs, 38 - 1 * xs, color='cyan', linestyle='dashed')
 # g.plot(xs, 45 - 0.67 * xs, color='cyan', linestyle='dashed')
 # g.plot(xs, 45 - 0.5 * xs, color='cyan', linestyle='dashed')
 
-g.plot(xs, 38 - 0.5 * xs, color='cyan', linestyle='dashed')
+# g.plot(xs, 38 - 0.5 * xs, color='cyan', linestyle='dashed')
 
 g.set_ylabel('depth')
 g.set_xlabel('n_hidden')
 
 plt.title('AR full')
-plt.savefig(f'fig/ar_mlp_depth_v_size_long.png', bbox_inches='tight')
+# plt.savefig(f'fig/ar_mlp_depth_v_size_long.png', bbox_inches='tight')
 plt.show()
 
 # <codecell>

experiment/remote/12_scale/ar_arm/run.py
@@ -18,7 +18,7 @@ print('RUN ID', run_id)
 
 run_split = 36
 
-train_iters = 100_000
+train_iters = 200_000
 # warmup_iters = 2000
 
 depth = 10
@@ -26,30 +26,27 @@ n_hop = 5
 n_hops_test = [5, 6, 7, 8, 9]
 
 all_n_layer = [1]
-switch = [True]  # resid
 
 n_arms = (2**np.linspace(3, 9, num=40)).astype(int) * 2
 n_widths = (2**np.linspace(3, 9, num=40)).astype(int) * 2
 
-lrs = [1e-2]
-gammas = [1, 10, 100]
+lrs = [5e-3]
+gammas = [1]
 
 ### START TEST CONFIGS
 # run_split = 1
-# train_iters = 500
-# warmup_iters = 100
+# train_iters = 10_000
 
 # depth = 10
 # n_hop = 5
 # n_hops_test = [7]
 
-# all_n_layer = [2]
-# switch = [True]
+# all_n_layer = [1]
 # n_arms = [100]
 # n_widths = [256]
 
-# lrs = [1e-2]
-# gammas = [10]
+# lrs = [5e-3]
+# gammas = [1]
 ### END TEST CONFIGS
 
 all_cases = []
@@ -101,24 +98,6 @@ for lr, gamma, n_arm, n_hidden in itertools.product(lrs, gammas, n_arms, n_width
 
         return StarfishTask(cot=cot, trace_to_start=ttr, **task_args, **kwargs)
     
-
-    # all_cases.extend([
-    #     Case(f'Zero',
-    #             TransformerConfig(n_heads=1,
-    #                             n_out=1,
-    #                             n_layers=1,
-    #                             pos_emb=False, 
-    #                             return_format='final_logit',
-    #                             n_mlp_layers=2,
-    #                             layer_norm=False,
-    #                             residual_connections=True,
-    #                             mup_scale=True,
-    #                             linear_att=False,
-    #                             **model_args),
-    #             train_args=make_train_args('bce'),
-    #             train_task=make_chain(cot=False)
-    #     ), 
-    # ])
 
     all_cases.extend([
         Case(f'AR (direct)',

experiment/remote/12_scale/ar_length/run.py
@@ -21,7 +21,7 @@ run_split = 36
 batch_size = 128
 accum_k = 1
 
-train_iters = 100_000 * accum_k
+train_iters = 200_000 * accum_k
 
 n_arms = [10]
 n_depths = (2**np.linspace(3, 9, num=40)).astype(int) * 2