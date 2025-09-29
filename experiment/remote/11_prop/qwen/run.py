17ae03d3259c3eb5f5501fade6f267cba4b1aab8
William Tong <williamtong105@gmail.com>
2025-09-29 16:30:33 -0400

    Fix qwen run

--- experiment/remote/11_prop/qwen/run.py
+++ experiment/remote/11_prop/qwen/run.py
@@ -5,8 +5,8 @@ Big qwen run
 # <codecell>
 import datasets
 from peft import LoraConfig
-from transformers import AutoModelForCausalLM, TrainingArguments
-from trl import SFTTrainer
+from transformers import AutoModelForCausalLM, TrainingArguments, BitsAndBytesConfig
+from trl import SFTTrainer, SFTConfig
 import torch
 
 import sys
@@ -23,19 +23,22 @@ def make_ds(depth, split):
     ds = datasets.concatenate_datasets([task.true_ds, task.false_ds]).shuffle(seed=new_seed())
     return ds
 
-train_ds = make_ds(10, 'train')
-test_ds = make_ds(10, 'test')
+train_ds = make_ds(6, 'train')
+test_ds = make_ds(6, 'test')
 
-
-model = AutoModelForCausalLM(
-    model_name,
-    torch_dtype=torch.bfloat16,
-    device_map='auto',
-    attn_implementation='flash_attention_2',
+quant_config = BitsAndBytesConfig(
     load_in_4bit=True,
     bnb_4bit_use_double_quant=True,
     bnb_4bit_compute_dtype=torch.bfloat16,
     bnb_4bit_quant_type='nf4'
+)
+
+model = AutoModelForCausalLM.from_pretrained(
+    model_name,
+    dtype=torch.bfloat16,
+    device_map='auto',
+    attn_implementation='flash_attention_2',
+    quantization_config=quant_config
 )
 
 peft_config = LoraConfig(
@@ -45,8 +48,8 @@ peft_config = LoraConfig(
     target_modules=("q_proj","k_proj","v_proj","o_proj","gate_proj","up_proj","down_proj")
 )
 
-args = TrainingArguments(
-    output_dir="qwen25_coder7b_prop_qlora", # TODO: pick destination
+args = SFTConfig(
+    output_dir="~/scratch/qwen25_coder7b_prop_qlora", # TODO: pick destination
     num_train_epochs=5,
     per_device_train_batch_size=8,
     gradient_accumulation_steps=8,      
@@ -57,10 +60,11 @@ args = TrainingArguments(
     save_steps=500,
     bf16=True,                           
     gradient_checkpointing=True,
-    optim="adamw_bnb_8bit",              # QLoRA-friendly optimizer
+    optim="adamw_bnb_8bit",              # QLoRA-friendly optimizer. Consider paging?
     max_grad_norm=0.3,
     weight_decay=0.0,
     report_to="none",
+    completion_only_loss=True,
 )
 
 trainer = SFTTrainer(
@@ -69,7 +73,6 @@ trainer = SFTTrainer(
     args=args,
     train_dataset=train_ds,
     eval_dataset=test_ds,
-    completion_only_loss=True,
     packing=True,             # pack many short samples to fill context
     max_seq_length=4096,      # TODO: confirm
 )
