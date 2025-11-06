8472906b09f6a57d6a4b1f8bb6a29a866550116b
William Tong <williamtong105@gmail.com>
2025-11-06 11:02:27 -0500

    Prepare eval script for general sweep

--- experiment/remote/14_qwen_sweep/qwen/sb_eval.sh
+++ experiment/remote/14_qwen_sweep/qwen/sb_eval.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
 #SBATCH -c 16
 #SBATCH -t 6:00:00
-#SBATCH -p kempner_h100
+#SBATCH -p kempner_requeue
 #SBATCH --gres=gpu:1
 #SBATCH --mem=128000
 #SBATCH -o log.%A.%a.out
 #SBATCH -e log.%A.%a.err
-#SBATCH --array=1-12
+#SBATCH --array=1-36
 #SBATCH --mail-type=END
 #SBATCH --mail-user=wtong@g.harvard.edu
 #SBATCH --account=kempner_pehlevan_lab
+#SBATCH --constraint="h100|h200"
 
 
 module load gcc
