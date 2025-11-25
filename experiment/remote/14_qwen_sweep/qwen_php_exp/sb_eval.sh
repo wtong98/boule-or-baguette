220a500738fa3576b6051d95df5b6a4f62eb4bcb
wtong98 <wtong@g.harvard.edu>
2025-11-25 15:49:47 -0500

    Update submit scripts

--- experiment/remote/14_qwen_sweep/qwen_php_exp/sb_eval.sh
+++ experiment/remote/14_qwen_sweep/qwen_php_exp/sb_eval.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 #SBATCH -c 16
 #SBATCH -t 12:00:00
-#SBATCH -p kempner_requeue
+# #SBATCH -p kempner_requeue
+#SBATCH -p kempner_h100
 #SBATCH --gres=gpu:1
 #SBATCH --mem=128000
 #SBATCH -o log_eval.%A.%a.out
 #SBATCH -e log_eval.%A.%a.err
-#SBATCH --array=1-18
+#SBATCH --array=1-18%12
 #SBATCH --mail-type=END
 #SBATCH --mail-user=wtong@g.harvard.edu
 #SBATCH --account=kempner_grads
