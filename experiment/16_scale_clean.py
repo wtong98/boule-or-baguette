b50a7fa0f4dfa2a0f1e56e8fa5f2bc672c6f1a72
William Tong <williamtong105@gmail.com>
2025-12-05 09:57:51 -0500

    Prepare gen runs

--- experiment/16_scale_clean.py
+++ experiment/16_scale_clean.py
@@ -286,3 +286,88 @@ g.set_xlabel('Hidden (H)')
 
 plt.title('AR depth')
 plt.show()
+
+
+# <codecell>
+df = collate_dfs('remote/16_scale_clean/gen/', show_progress=True)
+df
+
+# <codecell>
+rand_idxs = np.random.choice(len(df), size=100, replace=False)
+for ex in df['hist'].iloc[rand_idxs]:
+    vals = [p['loss'] for p in ex['test']]
+    plt.plot(vals, color='C0', alpha=0.1)
+
+# df['hist'].iloc[0]['train']
+    
+
+# %%
+def extract_plot_vals(row):
+    n_hop_prop = row['info']['n_hop_prop']
+    del row['info']['n_hop_prop']
+    del row['info']['n_hop']
+
+    return pd.Series([
+        row['name'],
+        row['train_task'].n_arms,
+        row['train_task'].depth,
+        row['train_task'].samp_dist[1],
+        row['config']['n_hidden'],
+        row['config']['n_layers'],
+        row['hist']['test'][5]['acc'],
+        n_hop_prop,
+        row['info'],
+    ], index=['name', 'n_arms', 'depth', 'n_hop', 'n_hidden', 'n_layers', 'acc_hist', 'n_hop_prop', 'info'])
+
+plot_df = df.copy().apply(extract_plot_vals, axis=1) \
+            .reset_index(drop=True) \
+
+adf = pd.DataFrame(plot_df['info'].tolist()) \
+        .stack() \
+        .reset_index(level=1, name='info')
+
+plot_df = plot_df.drop('info', axis=1) \
+                 .join(adf) \
+                 .rename(columns={'level_1': 'test_n_hop'}) \
+                 .reset_index(names='orig_index')
+
+bdf = pd.DataFrame(plot_df['info'].tolist())
+# bdf.loc[~pd.isna(bdf['gen_acc']),'acc'] = bdf[~pd.isna(bdf['gen_acc'])]['gen_acc']
+# bdf = bdf.drop('gen_acc', axis=1)
+
+plot_df = pd.concat((plot_df.drop('info', axis=1), bdf), axis=1)
+plot_df
+
+# <codecell>
+mdf = plot_df.copy()
+
+depth = 30
+n_hop_prop = 0.25
+
+train_split = np.round(depth * n_hop_prop)
+
+mdf = mdf[
+    (mdf['n_hop_prop'] == n_hop_prop)
+    & (mdf['depth'] == depth)
+]
+
+g = sns.lineplot(mdf, x='test_n_hop', y='acc', hue='n_arms', style='name')
+g.axvline(x=train_split, color='red', linestyle='dashed', alpha=0.7)
+g.set_title('DP generalization')
+
+# plt.savefig(f'fig/dp_gen_depth_{depth}_hop_{n_hop_prop}.png', bbox_inches='tight')
+
+# <codecell>
+adf = mdf[mdf['test_n_hop'] < 15]
+# adf = mdf.copy()
+
+g = sns.lineplot(adf, x='n_arms', y='acc', hue='test_n_hop', alpha=0.5, marker='o')
+g.set_xscale('log')
+g.set_yscale('log')
+
+xs = np.linspace(np.log(8), np.log(27))
+g.plot(np.exp(xs), np.exp(-0.5 * xs + 1), color='red', linestyle='dashed')
+g.text(12, 0.8, r'$\propto B^{-1/2}$', color='red')
+# g.plot(np.exp(xs), np.exp(-1 * xs + 2), color='blue', linestyle='dashed')
+
+# plt.savefig(f'fig/dp_gen_arms_pred_depth_{depth}_hop_{n_hop_prop}.png', bbox_inches='tight')
---
+++ experiment/remote/16_scale_clean/gen/run.py
@@ -0,0 +1,174 @@
+"""
+Relationship between size and accuracy on trees of various size
+"""
+
+# <codecell>
+import pandas as pd
+from tqdm import tqdm
+
+import sys
+sys.path.append('../../../../')
+from common import *
+from train import *
+from model.transformer import TransformerConfig
+from task.graph import *
+
+run_id = new_seed()
+print('RUN ID', run_id)
+
+run_split = 16
+
+train_iters = 100_000
+
+# n_arms = [2, 3, 4, 5, 6, 8, 10, 15, 20, 30, 50]
+n_arms = np.arange(2, 31, step=4)
+n_depths = [30]
+n_widths = [4096]
+
+n_hop_props = [0.25]
+lrs = [1e-2]
+
+all_n_layer = [1]
+max_batch_size = 128
+
+seed = 50
+
+
+### START TEST CONFIGS
+# run_split = 1
+# train_iters = 50
+
+# n_arms = [2]
+# n_depths = [10]
+# n_widths = [32]
+
+# all_n_layer = [1]
+# n_hop_props = [0.25]
+# lrs = [1e-2]
+
+# max_batch_size = 128
+### END TEST CONFIGS
+
+all_cases = []
+
+eval_fns = [loss_and_acc, gen_acc_cot1]
+
+for lr, n_hop_prop, n_arm, depth, n_hidden, n_layer in itertools.product(lrs, n_hop_props, n_arms, n_depths, n_widths, all_n_layer):
+    n_hop = np.round(n_hop_prop * depth).astype(int)
+    n_vocab = n_arm * depth + 1 + StarfishTask.offset
+
+    batch_size = n_arm * depth
+    batch_size, k = split_batch(batch_size, max_batch_size)
+
+    model_args = {
+        'n_vocab': n_vocab,
+        'n_hidden': n_hidden,
+        'use_bias': False,
+        'freeze_emb': True,
+        'dtype': jnp.bfloat16
+    }
+
+    def make_train_args(loss='ce_mask'):
+        args = {
+            'loss': loss,
+            'test_every': 1000,
+            'train_iters': train_iters,
+            'lr': lr / n_layer,
+            'k': k
+        }
+
+        args['eval_fns'] = [loss_and_acc]
+
+        if loss == 'ce_mask':
+            args['eval_fns'] = eval_fns
+            args['print_fn'] = print_gen
+        
+        return args
+
+
+    def make_chain(cot=True, ttr=False):
+        task_args = {
+            'depth': depth,
+            'samp_dist': (1, n_hop),
+            'n_arms': n_arm,
+            'nouveau': True,
+            'batch_size': batch_size
+        }
+
+        return StarfishTask(cot=cot, trace_to_start=ttr, **task_args)
+    
+
+    all_cases.extend([
+        Case(f'AR',
+                TransformerConfig(n_heads=1,
+                                n_out=n_vocab,
+                                n_layers=1,
+                                pos_emb=False, 
+                                return_format=None,
+                                n_mlp_layers=2,
+                                layer_norm=False,
+                                residual_connections=False,
+                                mup_scale=True,
+                                linear_att=False,
+                                unif_att=False,
+                                **model_args),
+                train_args=make_train_args('ce_mask'),
+                train_task=make_chain(cot=True, ttr=True),
+                info={'n_hop_prop': n_hop_prop, 'n_hop': n_hop}
+        ), 
+
+        Case(f'DP',
+                TransformerConfig(n_heads=1,
+                                n_out=1,
+                                n_layers=n_layer,
+                                pos_emb=False, 
+                                return_format='final_logit',
+                                n_mlp_layers=2,
+                                layer_norm=False,
+                                residual_connections=False,
+                                mup_scale=True,
+                                linear_att=False,
+                                unif_att=False,
+                                **model_args),
+                train_args=make_train_args('bce'),
+                train_task=make_chain(cot=False),
+                info={'n_hop_prop': n_hop_prop, 'n_hop': n_hop}
+        ), 
+    ])
+
+    
+all_cases = split_cases(all_cases, run_split, shuffle_seed=seed)
+print('CASES', all_cases)
+
+
+for case in tqdm(all_cases):
+    print('RUNNING', case.name)
+    case.run()
+
+    tt = case.train_task
+    for n_hop in range(2, tt.depth):
+        test_task = StarfishTask(n_arms=tt.n_arms, 
+                                 depth=tt.depth, 
+                                 samp_dist=n_hop, 
+                                 cot=tt.cot, 
+                                 trace_to_start=tt.trace_to_start, 
+                                 nouveau=tt.nouveau,
+                                 batch_size=1024)
+
+        case.eval(
+            test_task,
+            eval_fns=case.train_args['eval_fns'],
+            prefix=n_hop,
+            n_iters=1
+        )
+
+    case.state = None
+    case.train_args['eval_fns'] = None
+
+
+df = pd.DataFrame(all_cases)
+df.to_pickle(f'res.{run_id}.pkl')
+
+print('done!')
+
+# %%
