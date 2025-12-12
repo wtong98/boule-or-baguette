2017792f0be1b7ea8863a2b33c18718a6124b577
William Tong <williamtong105@gmail.com>
2025-12-12 10:44:31 -0500

    Add interval evaluation

--- experiment/remote/15_llm_sweep/gemma_php_par/eval.py
+++ experiment/remote/15_llm_sweep/gemma_php_par/eval.py
@@ -105,6 +105,14 @@ range_hops = [1] + [h + 1 for h in test_splits] + [np.inf]
 ranges = list(zip(range_hops[:-1], range_hops[1:]))
 val_ds_set = [make_ds(ds_path, r, split='range') for r in ranges]
 
+if len(val_ds_set) > 2:
+    interval = (range_hops[1], np.inf)
+    print('BIG TEST INTERVAL', interval)
+    full_val_ds = make_ds(ds_path, interval, split='range')
+
+    ranges.append(interval)
+    val_ds_set.append(full_val_ds)
+
 model_name = run_config['model_name']
 max_length = run_config['max_test_length'] if 'max_test_length' in run_config else run_config['max_length']
 
