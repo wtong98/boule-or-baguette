695fca6686b3552815b7bbf1ea787807f53e237c
William Tong <williamtong105@gmail.com>
2026-01-19 10:18:17 -0500

    Prepare Llama or runs

---
+++ experiment/remote/17_llm_clean/llama_or/config.py
@@ -0,0 +1,102 @@
+"""Configs for model sweeps"""
+
+# <codecell>
+import itertools
+import sys
+
+sys.path.append('../')
+from run import perform_run
+
+data_dir = '/n/netscratch/pehlevan_lab/Lab/wlt/data/prop_gen/data'
+
+perform = perform_run
+if sys.argv[-1] == 'eval':
+    from eval import perform_eval
+    perform = perform_eval
+else:
+    if sys.argv[-1] != 'run':
+        print(f"unrecognized mode: {sys.argv[-1]}, defaulting to run")
+
+
+itr_ids = list(range(10))
+base_len = 1024
+
+model_sets = [
+    {'name': 'meta-llama/Llama-3.2-1b-pt', '2k_bs': 32, '32k_bs': 1},
+    {'name': 'meta-llama/Llama-3.2-3b-pt', '2k_bs': 32, '32k_bs': 1},
+    {'name': 'meta-llama/Llama-3.1-8b-pt', '2k_bs': 32, '32k_bs': 1},
+]
+
+prompt_styles = [
+    'ar_cot',
+    'dp',
+]
+
+tasks = [
+    {'name': 'or', 'train_len': 8192, 'test_len': 32_768},
+]
+
+task_to_ds_path = {
+    'or': data_dir + '/hf_or_par_text_32768_pipe',
+}
+
+# Corresponds roughly to 50, 90th percentiles
+task_to_splits = {
+    'or': [18],
+}
+
+task_to_test_splits = {
+    'or': [18],
+}
+
+
+configs = []
+
+for i in itr_ids:
+    for model_set, task_args, prompt in itertools.product(model_sets, tasks, prompt_styles):
+        total_batch_size = 32
+        model_name = model_set['name']
+        task = task_args['name']
+
+        batch_size = model_set['2k_bs']
+
+        len_scale_fac = base_len / task_args['train_len']
+        batch_size = min(int(batch_size * len_scale_fac), total_batch_size)
+
+        accum_steps = total_batch_size // batch_size
+        if accum_steps < 1:
+            accum_steps = 1
+            batch_size = total_batch_size
+        
+
+        base_config = {
+            'model_name': model_name,
+            'task_name': task,
+            'batch_size': batch_size,
+            'accum_steps': accum_steps,
+            'max_steps': 2_500,
+            'num_samples': 16,
+            'max_length': task_args['train_len'],
+            'max_test_length': task_args['test_len'],
+            'log_every': 250 if prompt == 'dp' else 999_999_999,
+            'save_every': 250,
+            'ds_path': task_to_ds_path[task],
+            'splits': task_to_splits[task],
+            'test_splits': task_to_test_splits[task],
+            'prompt': prompt,
+            'project_name': f'llama_{task}',
+            'run_name_prefix': f"{model_name.split('/')[-1]}-{prompt}",
+            'packing': False
+        }
+
+        for split in base_config['splits']:
+            curr = base_config.copy()
+            curr['train_split'] = split
+            curr['run_name'] = f"{base_config['run_name_prefix']} (split={split}, itr={i})"
+            curr['output_dir'] = f"~/scratch/ckpt/final/{curr['project_name']}/{curr['run_name'].replace(' ', '_')}"
+            configs.append(curr)
+
+len(configs)
+# <codecell>
+
+perform(configs)
